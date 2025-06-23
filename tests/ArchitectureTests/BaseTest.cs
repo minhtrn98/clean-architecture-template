5b11cc8eeb978d0edfea259f28258981c9a6c2e6
minhtrn98 <minhtrn98@gmail.com>
2025-06-23 12:15:19 +0700

    fix: Update ApplicationAssembly reference to use DependencyInjection type

--- tests/ArchitectureTests/BaseTest.cs
+++ tests/ArchitectureTests/BaseTest.cs
@@ -8,7 +8,7 @@ namespace ArchitectureTests;
 public abstract class BaseTest
 {
     protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
-    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
     protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
     protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
 }
